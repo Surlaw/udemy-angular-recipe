ec0fcecdb90433bbb5f9b0add38bf7083b390c7d
Surlaw <freyns@gmail.com>
2022-04-23 13:34:48 +0200

    Initial commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Rainbow Cake',
+      'Colorful',
+      'https://bakerbynature.com/wp-content/uploads/2021/09/17-Stunning-birthday-cake-recipes-19q-1-of-1.jpg'
+    ),
+    new Recipe(
+      'Brownie',
+      'Chocolate',
+      'https://bakerbynature.com/wp-content/uploads/2021/09/17-Stunning-birthday-cake-recipes-7-1-of-1.jpg'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+}

2490866a6a6d6b840fc542f35d99058f8481d528
Surlaw <freyns@gmail.com>
2022-05-01 12:15:33 +0200

    8 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,11 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { AppRoutingModule } from './app-routing.module';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   declarations: [
     AppComponent,
     HeaderComponent,
@@ -25,6 +27,7 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
+    RecipeStartComponent,
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent],
